fc520f24ad060a7a3fcc931f915b5f783e1d862a
thinh-tr <ducthinh2049@gmail.com>
2026-03-18 15:53:36 +0700

    fix: bỏ các namespace thừa khỏi mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-export * as language from "./src/support-languages.ts"; // các thành phần của support-languages
-export * as helperfuncs from "./src/helper-funcs.ts";   // các thành phần của helper-funcs
-export * as searchdatamodels from "./src/search-data-models.ts";    // các thành phần của search-data-models
+export * from "./src/support-languages.ts"; // các thành phần của support-languages
+export * from "./src/helper-funcs.ts";   // các thành phần của helper-funcs
+export * from "./src/search-data-models.ts";    // các thành phần của search-data-models
